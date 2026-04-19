66e94f81098b968d8ac3bb788c347cb09e4ec85c
DinhThuc1012 <thithuc1012@gmail.com>
2026-04-19 19:28:23 +0700

    Update urls.py

--- Backend/mobile/studezy/urls.py
+++ Backend/mobile/studezy/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LoginAPIView, RegisterAPIView, HomeSummaryAPIView, ClassesTodayAPIView, TopDeadlinesAPIView, ClassesByDateAPIView, DeadlinesByDateAPIView, ToggleDeadlineAPIView
+from .views import LoginAPIView, RegisterAPIView, HomeSummaryAPIView, ClassesTodayAPIView, TopDeadlinesAPIView, ClassesByDateAPIView, DeadlinesByDateAPIView, ToggleDeadlineAPIView, TasksAPIView, UpdateTaskStatusAPIView, EditTaskAPIView, DeleteTaskAPIView
 
 urlpatterns = [
     path('api/login/', LoginAPIView.as_view(), name='api-login'),
@@ -10,8 +10,8 @@ urlpatterns = [
     path('api/classes-by-date/', ClassesByDateAPIView.as_view()),
     path('api/deadlines-by-date/', DeadlinesByDateAPIView.as_view()),
     path('api/deadlines/<int:pk>/toggle/', ToggleDeadlineAPIView.as_view()),
-
-
-
-
+    path('api/tasks/', TasksAPIView.as_view(), name='api-tasks'),
+    path('api/tasks/<int:pk>/update-status/', UpdateTaskStatusAPIView.as_view(), name='api-update-task-status'),
+    path('api/tasks/<int:pk>/edit/', EditTaskAPIView.as_view(), name='api-edit-task'),
+    path('api/tasks/<int:pk>/delete/', DeleteTaskAPIView.as_view(), name='api-delete-task'),
 ]
